61da7ac96f65e2232f8265dbc80d472b0145d399
Manish Verma <manish.verma604@gmail.com>
2024-09-30 21:45:32 +0530

    Folder List screen implemented.

---
+++ DailyDiary/app/src/main/java/com/manish/app/dailydiary/ui/screens/FolderListScreen.kt
@@ -0,0 +1,85 @@
+package com.manish.app.dailydiary.ui.screens
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.manish.app.dailydiary.R
+
+class FolderListScreen {
+
+
+    @Composable
+    fun TopBar(modifier: Modifier = Modifier) {
+        Row {
+            Text(text = "folders",
+                textAlign = TextAlign.Start,
+                modifier = Modifier
+                    .padding(start = 20.dp, top = 64.dp)
+                    .weight(1f),
+                fontSize = 32.sp)
+
+            Image(painter = painterResource(id = R.drawable.calendar_month_24),
+                modifier = Modifier.padding(top = 74.dp, end = 32.dp),
+                contentDescription = "sort_icon"
+            )
+
+        }
+    }
+
+    @Composable
+    fun ShowDiaryFolderList() {
+        //TODO: Fetch folder data from dB and show in LazyColumn
+        LazyColumn(modifier = Modifier.padding(top = 120.dp)) {
+            items(20) {
+                ListItems()
+            }
+        }
+    }
+
+    @Composable
+    private fun ListItems() {
+        val paddingModifier  = Modifier
+            .padding(start = 20.dp, end = 20.dp, top = 5.dp, bottom = 5.dp)
+            .fillMaxWidth()
+        Card(elevation = CardDefaults.cardElevation(),
+            modifier = paddingModifier) {
+            Column {
+                Row {
+                    Text(text = "personal notes",
+                        modifier = Modifier.padding(10.dp)
+                            .alpha(0.5f)
+                            .weight(0.85f),
+                        fontSize = 12.sp)
+
+                    Image(painter = painterResource(id = R.drawable.keyboard_arrow_right_24),
+                        modifier = Modifier
+                            .padding(10.dp)
+                            .weight(0.15f),
+                        contentDescription = "next_arrow"
+                    )
+                }
+                Spacer(modifier = Modifier.height(60.dp))
+                Text(text = "25",
+                    modifier = Modifier.padding(10.dp),
+                    fontSize = 36.sp)
+            }
+
+        }
+    }
+}
